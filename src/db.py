b202c36c84bbda68225a52713905c139e6262a53
NicoWoch <woch.nico@gmail.com>
2022-08-06 17:08:41 +0200

    Refactored db.py and added caching

--- src/db.py
+++ src/db.py
@@ -1,51 +1,100 @@
 import logging
 import os.path
 import pickle
+from typing import Optional, Any
 
 from src.algorithms.tournament import Tournament
 from src.config import Config
 from src.player import Player
 
 
+class DB:
+    def __init__(self):
+        self.players_cache: Optional[list[Player]] = None
+        self.tournaments_cache: Optional[list[Tournament]] = None
+
+    def _load_object(self, filepath: str, default: Any) -> Any:
+        if not os.path.exists(filepath):
+            logging.debug(f'Creating file "{filepath}"')
+            self._save_object(filepath, default)
+
+        logging.debug(f'Loading object from file "{filepath}"')
+
+        try:
+            file = open(filepath, 'rb')
+            return pickle.load(file)
+        except OSError:
+            logging.critical(f'Cannot open file "{filepath}" in "rb" mode')
+            raise
+
+    def _save_object(self, filepath: str, obj: Any):
+        logging.debug(f'Saving object to file "{filepath}"')
+
+        if not os.path.exists(filepath):
+            os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+        try:
+            file = open(filepath, 'wb')
+            pickle.dump(obj, file)
+        except OSError:
+            logging.critical(f'Cannot open file "{filepath}" in "wb" mode')
+            raise
+
+    def _test_players(self, players):
+        assert isinstance(players, list), f'Players Testing Error ({type(players) = } != list)'
+        if len(players) != 0:
+            assert isinstance(players[0], Player), f'Players Testing Error ({type(players[0]) = } != Player)'
+
+    def _test_tournaments(self, tournaments):
+        assert isinstance(tournaments, list), f'Tournaments Testing Error ({type(tournaments) = } != list)'
+        if len(tournaments) != 0:
+            assert isinstance(tournaments[0], Tournament), f'Tournaments Testing Error ({type(tournaments[0]) = } != Tournament)'
+
+    def load_players(self) -> list[Player]:
+        if self.players_cache is not None:
+            logging.debug('Loading players from cache')
+            return self.players_cache
+
+        players = self._load_object(Config.DB_PLAYERS, [])
+        self._test_players(players)
+        self.players_cache = players
+        return players
+
+    def save_players(self, players: list[Player]):
+        self._test_players(players)
+        self.players_cache = players
+        self._save_object(Config.DB_PLAYERS, players)
+
+    def load_tournaments(self) -> list[Tournament]:
+        if self.tournaments_cache is not None:
+            logging.debug('Loading tournaments from cache')
+            return self.tournaments_cache
+
+        tournaments = self._load_object(Config.DB_TOURNAMENTS, [])
+        self._test_tournaments(tournaments)
+        self.tournaments_cache = tournaments
+        return tournaments
+
+    def save_tournaments(self, tournaments: list[Tournament]):
+        self._test_tournaments(tournaments)
+        self.tournaments_cache = tournaments
+        self._save_object(Config.DB_TOURNAMENTS, tournaments)
+
+
+MAIN_DB = DB()
+
+
 def get_players(*, debug=True) -> list[Player]:
-    if not os.path.exists(Config.DB_PLAYERS):
-        logging.info(f'Creating players database in file "{Config.DB_PLAYERS}"')
-        save_players([], debug=False)
-
-    if debug:
-        logging.info(f'Loading players from file "{Config.DB_PLAYERS}"')
-
-    players = pickle.load(open(Config.DB_PLAYERS, 'rb'))
-    assert isinstance(players, list), 'Importing Error (other instance)'
-    assert len(players) == 0 or isinstance(players[0], Player), 'Importing Error (other instance in list)'
-    return players
+    return MAIN_DB.load_players()
 
 
 def save_players(players: list[Player], *, debug=True):
-    if debug:
-        logging.info(f'Saving players to file "{Config.DB_PLAYERS}"')
-
-    os.makedirs(os.path.dirname(Config.DB_PLAYERS), exist_ok=True)
-    pickle.dump(players, open(Config.DB_PLAYERS, 'wb'))
+    MAIN_DB.save_players(players)
 
 
 def get_tournaments(*, debug=True) -> list[Tournament]:
-    if not os.path.exists(Config.DB_TOURNAMENTS):
-        logging.info(f'Creating tournaments database in file "{Config.DB_PLAYERS}"')
-        save_tournaments([], debug=False)
-
-    if debug:
-        logging.info(f'Loading tournaments from file "{Config.DB_TOURNAMENTS}"')
-
-    tournaments = pickle.load(open(Config.DB_TOURNAMENTS, 'rb'))
-    assert isinstance(tournaments, list), 'Importing Error (other instance)'
-    assert len(tournaments) == 0 or isinstance(tournaments[0], Tournament), 'Importing Error (other instance in list)'
-    return tournaments
+    return MAIN_DB.load_tournaments()
 
 
 def save_tournaments(tournaments: list[Tournament], *, debug=True):
-    if debug:
-        logging.info(f'Saving tournaments to file "{Config.DB_TOURNAMENTS}"')
-
-    os.makedirs(os.path.dirname(Config.DB_TOURNAMENTS), exist_ok=True)
-    pickle.dump(tournaments, open(Config.DB_TOURNAMENTS, 'wb'))
+    MAIN_DB.save_tournaments(tournaments)
