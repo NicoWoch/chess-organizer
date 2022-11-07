3d76e665a2f5c74f96d208e9be30a6307ac11802
NicoWoch <woch.nico@gmail.com>
2022-11-07 20:39:44 +0100

    Changed sorting players in starting list

--- src/gui/tournament/pairs_frame.py
+++ src/gui/tournament/pairs_frame.py
@@ -96,7 +96,7 @@ class PairsFrame(tk.Frame):
     def update_first(self, tournament: Tournament):
         self.table.set_columns(*FIRST_COLUMNS)
 
-        self.first_page_players = sorted(tournament.players, key=lambda p: p.rating, reverse=True)
+        self.first_page_players = sorted(tournament.players, key=lambda p: (p.name, p.surname))
 
         self._update_rows([(player, player.rating) for player in self.first_page_players])
         self._update_waiting(None)
