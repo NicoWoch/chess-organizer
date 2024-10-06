e181c2d7724b948163c216655c6ffbcc56ee4d4a
NicoWoch <woch.nico@gmail.com>
2024-10-06 14:41:01 +0200

    Solved some todos

--- src/gui/validation_utils.py
+++ src/gui/validation_utils.py
@@ -5,7 +5,7 @@ from typing import Callable, Any
 def tk_validator(validator: Callable) -> Callable[..., dict[str, Any]]:
     @wraps(validator)
     def wrapper(self, **kwargs):
-        vcmd = self.register(lambda text: validator(self, text, **kwargs))  # TODO: here kwargs error
+        vcmd = self.register(lambda text: validator(self, text, **kwargs))
 
         return {
             'validate': 'all',
@@ -18,6 +18,7 @@ def tk_validator(validator: Callable) -> Callable[..., dict[str, Any]]:
 @tk_validator
 def tk_unsigned_validator(_, text: str):
     return text.isdigit()
+
 
 @tk_validator
 def tk_unsigned_float_validator(_, text: str):
--- src/gui/widgets/resizing_widgets.py
+++ src/gui/widgets/resizing_widgets.py
@@ -1,31 +1,10 @@
 import tkinter as tk
+from abc import ABCMeta, abstractmethod
 
 from src.gui.widgets.resizing_font import ResizingFont
 
 
-class ResizingLabel(tk.Label):  # TODO: remove code repetition
-    def __init__(self, parent, relwidth=.8, relheight=.8, minsize=4, maxsize=100, **kwargs):
-        if kwargs.get('bg', None) in ('transparent', None):
-            kwargs['bg'] = parent.cget('background')
-
-        super().__init__(parent, **kwargs)
-
-        base_font = kwargs.get('font', ('Comic Sans MS',))
-        self._font = ResizingFont(self, self.get_text, base_font, relwidth,
-                                  relheight, minsize, maxsize)
-        self._font.resize_font()
-
-        self.config(font=self._font)
-
-    def get_text(self) -> str:
-        return self.cget('text')
-
-    def destroy(self):
-        self._font.unbind()
-        super().destroy()
-
-
-class ResizingButton(tk.Button):
+class ResizingFontTransparentWidget(tk.Widget, metaclass=ABCMeta):
     def __init__(self, parent, relwidth=.8, relheight=.8, minsize=4, maxsize=100, **kwargs):
         if kwargs.get('bg', None) in ('transparent', None):
             kwargs['bg'] = parent.cget('background')
@@ -38,7 +17,10 @@ class ResizingButton(tk.Button):
                                   relheight, minsize, maxsize)
         self._font.resize_font()
 
-        self.config(font=self._font)
+        self._set_font(self._font)
+
+    @abstractmethod
+    def _set_font(self, font: ResizingFont): ...
 
     def get_text(self) -> str:
         return self.cget('text')
@@ -46,3 +28,13 @@ class ResizingButton(tk.Button):
     def destroy(self):
         self._font.unbind()
         super().destroy()
+
+
+class ResizingLabel(ResizingFontTransparentWidget, tk.Label):
+    def _set_font(self, font: ResizingFont):
+        self.config(font=font)
+
+
+class ResizingButton(ResizingFontTransparentWidget, tk.Button):
+    def _set_font(self, font: ResizingFont):
+        self.config(font=font)
