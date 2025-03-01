c33724311a2314fe5d46f2316336e462b559c1fa
NicoWoch <woch.nico@gmail.com>
2025-03-01 22:45:36 +0100

    Denying negative rating and restricting non-numeric characters inside of rating field

--- src/gui/subwindows/player_editor_window.py
+++ src/gui/subwindows/player_editor_window.py
@@ -21,7 +21,7 @@ class PlayerEditorWindow(tk.Toplevel):
         self.name = tk.StringVar(value=player.name)
         self.surname = tk.StringVar(value=player.surname)
         self.gender = tk.StringVar(value=str(player.gender.value))
-        self.rating = tk.IntVar(value=player.rating)
+        self.rating = tk.StringVar(value=player.rating)
 
         self.update_player_label()
         self.make_main_frame()
@@ -38,16 +38,15 @@ class PlayerEditorWindow(tk.Toplevel):
 
         self.make_entry(main_frame, 1, 'Imie', self.name, bind=self.update_player_label)
         self.make_entry(main_frame, 2, 'Nazwisko', self.surname, bind=self.update_player_label)
-        self.make_entry(main_frame, 3, 'Ranking', self.rating)
+        self.make_entry(main_frame, 3, 'Ranking', self.rating, validate=self.__rating_validator)
 
         tk.Button(main_frame, text='Zapisz', command=self.save, height=2) \
             .grid(row=4, column=0, columnspan=2, pady=10, sticky='nesw')
 
         main_frame.place(x=15, y=15, relwidth=1, width=-30, relheight=1, height=-30)
 
-    @classmethod
-    def make_entry(cls, main_frame: tk.Frame, i: int, label: str, var: tk.Variable, bind=None):
-        tk.Label(main_frame, text=label)\
+    def make_entry(self, main_frame: tk.Frame, i: int, label: str, var: tk.Variable, bind=None, validate=None):
+        tk.Label(main_frame, text=label) \
             .grid(row=i, column=0)
 
         entry = tk.Entry(main_frame, textvariable=var)
@@ -55,6 +54,13 @@ class PlayerEditorWindow(tk.Toplevel):
 
         if bind is not None:
             entry.bind('<KeyRelease>', bind)
+
+        if validate is not None:
+            entry.configure(validate='all', validatecommand=(self.register(validate), '%P'))
+
+    @classmethod
+    def __rating_validator(cls, text: str) -> bool:
+        return text == '' or text.isdigit()
 
     def update_player_label(self, *_):
         self.player_label.set(f'Gracz {self.name.get()} {self.surname.get()}')
@@ -66,10 +72,13 @@ class PlayerEditorWindow(tk.Toplevel):
         if name == '' or surname == '':
             return
 
+        if self.rating.get() == '':
+            pass
+
         self.player.name = name
         self.player.surname = surname
         self.player.gender = Gender(self.gender.get())
-        self.player.rating = self.rating.get()
+        self.player.rating = int(self.rating.get())
 
         self.on_save()
         self.destroy()
