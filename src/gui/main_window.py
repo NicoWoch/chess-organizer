23ee937f98fc69c343b0b7e8b9a996a80b465f1a
NicoWoch <woch.nico@gmail.com>
2025-03-02 12:17:20 +0100

    Improved developer menu, ordered elements and added separators

--- src/gui/main_window.py
+++ src/gui/main_window.py
@@ -1,7 +1,9 @@
 import logging
+import re
 import tkinter as tk
 import traceback
 import random
+from typing import Callable
 
 from src import dummy_generator
 from src.algorithms.constants import Result
@@ -168,78 +170,106 @@ class MainWindow(tk.Tk):
 
     def make_dev_menu(self, menubar):
         devmenu = tk.Menu(menubar, tearoff=0)
+        entries: list[tuple[int, int, str, Callable]] = []
 
         for func_name in dir(self):
             func = getattr(self, func_name)
 
-            if func_name.startswith('_dev_') and callable(func):
-                display_name = func_name[5:].replace('_', ' ').title()
+            if re.match('^_dev.._', func_name) and callable(func):
+                group = ord(func_name[4])
+                order = ord(func_name[5])
+                display_name = func_name[7:].replace('_', ' ').title()
 
-                devmenu.add_command(label=display_name, command=func)
+                entries.append((group, order, display_name, func))
+
+        entries.sort(key=lambda x: x[:3])
+
+        prev_group = entries[0][0]
+        for group, _, name, func in entries:
+            if prev_group != group:
+                devmenu.add_separator()
+                prev_group = group
+
+            devmenu.add_command(label=name, command=func)
 
         menubar.add_cascade(label='Developer', menu=devmenu)
 
-    def _dev_hide_menu(self):
+    def _dev1a_hide_menu(self):
         logging.info('DEVELOPER MODE - OFF')
         self.make_menu()
 
-    def _dev_create_5_random_players_in_database(self):
+    def _dev2a_set_random_results_to_players(self):
+        if self.tournament_frame.tournament is None:
+            return
+
+        def random_result() -> Result:
+            return random.choice([
+                Result.White, Result.Black, Result.Draw
+            ])
+
+        for i in range(len(self.tournament_frame.tournament.get_round(-1))):
+            self.tournament_frame.tournament.set_result(i, random_result())
+
+        self.tournament_frame.set_result(random_result())
+
+    def __dev_push_sample_rounds(self, times: int = 1):
+        for _ in range(times):
+            if self.tournament_frame.tournament is None:
+                return
+
+            if not self.tournament_frame.tournament.is_started:
+                self.tournament_frame.next_round()
+
+            self._dev2a_set_random_results_to_players()
+            self.tournament_frame.next_round()
+
+    def _dev2b_push_2_sample_rounds(self):
+        self.__dev_push_sample_rounds(2)
+
+    def _dev2c_push_5_sample_rounds(self):
+        self.__dev_push_sample_rounds(5)
+
+    def _dev2d_push_10_sample_rounds(self):
+        self.__dev_push_sample_rounds(10)
+
+    @staticmethod
+    def _dev3a_create_5_random_players_in_database():
         dummy_players = dummy_generator.get_random_players(5)
         MainDB.save_players(MainDB.load_players() + dummy_players)
 
-    def _dev_create_15_random_players_in_database(self):
-        dummy_players = dummy_generator.get_random_players(15)
+    @staticmethod
+    def _dev3b_create_20_random_players_in_database():
+        dummy_players = dummy_generator.get_random_players(20)
         MainDB.save_players(MainDB.load_players() + dummy_players)
 
-    def _dev_add_15_random_players_from_database(self):
+    def _dev4a_add_random_player_to_tournament(self, population: int = 1):
         players = MainDB.load_players()
-        self.tournament_frame.add_players(random.sample(players, k=15))
+        self.tournament_frame.add_players(random.sample(players, k=min(population, len(players))))
 
-    def _dev_set_random_results_to_players(self):
-        if self.tournament_frame.tournament is None:
-            return
+    def _dev4b_add_5_random_players_to_tournament(self):
+        self._dev4a_add_random_player_to_tournament(5)
 
-        for i in range(len(self.tournament_frame.tournament.get_round(-1))):
-            self.tournament_frame.tournament.set_result(i, random.choice([
-                Result.White, Result.Black, Result.Draw
-            ]))
+    def _dev4c_add_15_random_players_to_tournament(self):
+        self._dev4a_add_random_player_to_tournament(15)
 
-    def _dev_push_sample_round(self):
-        if self.tournament_frame.tournament is None:
-            return
-
-        if not self.tournament_frame.tournament.is_started:
-            self.tournament_frame.next_round()
-
-        self._dev_set_random_results_to_players()
-        self.tournament_frame.next_round()
-
-    def _dev_push_sample_rounds_5(self):
-        for _ in range(5):
-            self._dev_push_sample_round()
-
-    def _dev_push_sample_rounds_10(self):
-        for _ in range(10):
-            self._dev_push_sample_round()
-
-    def _dev_clear_players(self):
-        MainDB.save_players([])
-
-    def _dev_create_dummy_tournament(self):
+    def _dev4d_create_test_tournament(self):
         tournaments = MainDB.load_tournaments()
         tournaments.append(dummy_generator.create_empty_tournament(len(tournaments)))
         MainDB.save_tournaments(tournaments)
+        self.tournament_frame.open_tournament(tournaments[-1])
 
-    def _dev_create_3_dummy_tournaments(self):
-        tournaments = MainDB.load_tournaments()
+    def _dev5a_remove_all_players(self):
+        players = self.tournament_frame.tournament.players
 
-        for _ in range(3):
-            tournaments.append(dummy_generator.create_empty_tournament(len(tournaments)))
+        for player in players[:]:
+            self.tournament_frame.tournament.remove_player(player)
 
-        MainDB.save_tournaments(tournaments)
+        self.tournament_frame.remove_players()
 
-    def _dev_clear_players_database(self):
+    @staticmethod
+    def _dev5b_clear_players_database():
         MainDB.save_players([])
 
-    def _dev_clear_tournaments_database(self):
+    def _dev5c_clear_tournaments_database(self):
         MainDB.save_tournaments([])
+        self.tournament_frame.close_tournament()
